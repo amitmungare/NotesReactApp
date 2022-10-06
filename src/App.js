e2f89514f427d3825021571e694ee389c535bf64
Amit Mungare <amitmungare@Amits-MacBook-Pro.local>
2022-10-07 01:11:07 +0530

    basics of notes app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import  Editor from './components/Editor'
+import Sidebar from './components/Sidebar'
+import {nanoid} from 'nanoid'
+import Split from 'react-split';
 
 function App() {
+
+  const [notes, setNotes] = useState([])
+  const [currentNoteId, setCurrentNoteId] = useState(
+    (notes[0] && notes[0].id) || ""
+  )
+
+  function createNewNote(){
+    const newNote = {
+      id:nanoid(),
+      body: "Add your note here"
+    }
+    setNotes(prevNote => [newNote, ...prevNote])
+    setCurrentNoteId(newNote.id)
+  }
+
+  function updateNote(text){
+    setNotes(oldNotes => oldNotes.map(oldNote =>{
+      return oldNote.id === currentNoteId
+      ?{...oldNote, body:text}
+      :oldNote
+    }))
+  }
+
+  function findCurrentNote(){
+    return notes.find(note =>{
+      return note.id ===currentNoteId
+    }) || notes[0]
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <main>
+        {
+            notes.length > 0 
+            ?
+            <Split 
+                sizes={[30, 70]} 
+                direction="horizontal" 
+                className="split"
+            >
+                <Sidebar
+                    notes={notes}
+                    currentNote={findCurrentNote()}
+                    setCurrentNoteId={setCurrentNoteId}
+                    newNote={createNewNote}
+                />
+                {
+                    currentNoteId && 
+                    notes.length > 0 &&
+                    <Editor 
+                        currentNote={findCurrentNote()} 
+                        updateNote={updateNote} 
+                    />
+                }
+            </Split>
+            :
+            <div className="no-notes">
+                <h1>You have no notes</h1>
+                <button 
+                    className="first-note" 
+                    onClick={createNewNote}
+                >
+                    Create one now
+                </button>
+            </div>
+            
+        }
+        </main>
+    
     </div>
   );
 }
